1e5ed0acfe9273e1283291773321977d9d6c47ef
Pratik Biswas <04biswaspratik@gmail.com>
2024-07-03 22:13:39 +0530

    spotify clone demo

---
+++ Spotify-Clone-Demo2/spotify-clone/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { StateProvider } from "./utils/StateProvider";
+import reducer, { initialState } from "./utils/Reducer";
+import { BrowserRouter as Router } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
+    </Router>
+  </React.StrictMode>
+);
